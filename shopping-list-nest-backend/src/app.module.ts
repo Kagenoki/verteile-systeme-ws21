ad244ab707e9335081f66914246b788b02bbb0b4
Eric Beller <erbeit02@hs-esslingen.de>
2022-01-08 18:40:18 +0100

    docker typeorm

--- shopping-list-nest-backend/src/app.module.ts
+++ shopping-list-nest-backend/src/app.module.ts
@@ -2,10 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShoppingItemModule } from './shopping-item/shopping-item.module';
+import { config as dotenvInit } from 'dotenv'
+import { TypeOrmModule } from '@nestjs/typeorm';
+dotenvInit();
 
 @Module({
-  imports: [ShoppingItemModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.POSTGRES_DB,
+
+      entities: [
+        // '**/*.entity{.ts,.js}'
+      ],
+
+      migrationsTableName: 'migration',
+
+      migrations: ['src/migration/*.ts'],
+
+      cli: {
+        migrationsDir: 'src/migration',
+      },
+
+      // ssl: this.isProduction(),
+    })
+    , ShoppingItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
