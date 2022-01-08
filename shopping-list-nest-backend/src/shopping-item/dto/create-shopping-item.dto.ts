3fdb98da86a53396b302242db946f828665143e7
Dennis Buehl <debuit00@hs-esslingen.de>
2022-01-08 19:35:09 +0100

    added create dto

--- shopping-list-nest-backend/src/shopping-item/dto/create-shopping-item.dto.ts
+++ shopping-list-nest-backend/src/shopping-item/dto/create-shopping-item.dto.ts
@@ -1,1 +1,17 @@
-export class CreateShoppingItemDto {}
+import {ApiProperty} from "@nestjs/swagger";
+import{IsDate, IsNumber, IsString, IsBoolean} from "class-validator";
+
+export class CreateShoppingItemDto {
+    @ApiProperty({description: 'Shopping list item'})
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    quantity: number;
+
+    @IsDate()
+    creationDate: Date;
+
+    @IsBoolean()
+    shoppingDone: boolean;
+}
