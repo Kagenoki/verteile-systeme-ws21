a8bb10710837d871184296a6773167d39bd81b2b
Eric Beller <erbeit02@hs-esslingen.de>
2022-01-08 19:04:47 +0100

    added logging for serverport

--- shopping-list-nest-backend/src/main.ts
+++ shopping-list-nest-backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config as dotenvInit } from 'dotenv'
+import { Logger } from '@nestjs/common';
+dotenvInit();
+const logger: Logger = new Logger('Main');
+const PORT: number = parseInt(process.env.PORT) || 3000;
 
-async function bootstrap() {
+async function bootstrap()
+{
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () =>
+  {
+    logger.log(`Server running on Port: ${PORT}`);
+  });
 }
 bootstrap();
